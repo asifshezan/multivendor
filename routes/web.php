08849862c6c9f52d48d9f6eb9aaafaa1b1c8d97c
unknown <asifshezan7@gmail.com>
2022-06-04 23:05:13 +0600

    Partner Migration & Route Complete

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\UserController;
-
+use App\Http\Controllers\Admin\PartnerController;
 
 Route::group(['prefix' => 'dashboard','middleware' => 'auth'], function() {
     Route::get('/',[ AdminController::class, 'index' ])->name('admin.dashboard.index');
@@ -18,6 +18,19 @@ Route::group(['prefix' => 'dashboard','middleware' => 'auth'], function() {
         Route::put('/update/{slug}', [ UserController::class, 'update'])->name('user.update');
         Route::get('/softdelete/{slug}',[ UserController::class, 'softdelete' ])->name('user.softdelete');
         Route::get('/delete/{slug}',[ UserController::class, 'destroy' ])->name('user.destroy');
+        Route::get('/suspend/{slug}',[ UserController::class, 'suspend' ])->name('user.suspend');
+    });
+
+
+    Route::group(['prefix' => 'partner'], function(){
+        Route::get('/', [ PartnerController::class, 'index'])->name('partner.index');
+        Route::get('/create', [ PartnerController::class, 'create'])->name('partner.create');
+        Route::post('/', [ PartnerController::class, 'store'])->name('partner.store');
+        Route::get('/show/{slug}', [ PartnerController::class, 'show'])->name('partner.show');
+        Route::get('/edit/{slug}', [ PartnerController::class, 'edit'])->name('partner.edit');
+        Route::put('/update/{slug}', [ PartnerController::class, 'update'])->name('partner.update');
+        Route::get('/softdelete/{slug}', [ PartnerController::class, 'softdelete'])->name('partner.softdelete');
+        Route::get('/delete/{slug}', [ PartnerController::class, 'delete'])->name('partner.delete');
         Route::get('/suspend/{slug}',[ UserController::class, 'suspend' ])->name('user.suspend');
     });
 });
