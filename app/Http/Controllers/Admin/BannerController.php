eba5917116f60b86bef9de19887e9bdc07a17b0f
unknown <asifshezan7@gmail.com>
2022-06-12 21:30:36 +0600

    Banner index setting

--- app/Http/Controllers/Admin/BannerController.php
+++ app/Http/Controllers/Admin/BannerController.php
@@ -4,8 +4,22 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Banner;
+use Illuminate\Support\Str;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class BannerController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index(){
+        $banners = Banner::where('banner_status',1)->where('banner_id','DESC')->get();
+        return view('admin.banner.index', compact('banners'));
+    }
 }
