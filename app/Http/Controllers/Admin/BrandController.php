9a83acbe3df1a4dc95bfd6ac25c3ddaceeda8b74
unknown <asifshezan7@gmail.com>
2022-06-13 20:08:23 +0600

    Brand Index setup

--- app/Http/Controllers/Admin/BrandController.php
+++ app/Http/Controllers/Admin/BrandController.php
@@ -4,8 +4,28 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Brand;
+use Illuminate\Support\Str;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use Intervention\Image\ImageManagerStatic as Image;
+
+
 
 class BrandController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index(){
+        $all = Brand::where('brand_status',1)->orderBy('brand_id','DESC')->get();
+        return view('admin.brand.index', compact('all'));
+    }
+
+    public function create(){
+        return view('admin.brand.create');
+    }
 }
