b92d2d797a359978ce736f64fb6a284f6768c8bf
unknown <asifshezan7@gmail.com>
2022-06-13 20:52:08 +0600

    Brand Update setup

--- app/Http/Controllers/Admin/BrandController.php
+++ app/Http/Controllers/Admin/BrandController.php
@@ -49,6 +49,7 @@ class BrandController extends Controller
             'brand_remarks' => $request->brand_remarks,
             'brand_creator' => $creator,
             'brand_feature' => $brand_feature,
+            'brand_status' => 1,
             'created_at' => Carbon::now()->toDateTimeString()
         ]);
 
@@ -67,6 +68,55 @@ class BrandController extends Controller
             return redirect()->back();
         }else{
             Session::flash('error','Opps! Failed to insert.');
+            return redirect()->back();
+        }
+    }
+
+    public function edit($slug){
+        $brand = Brand::where('brand_status',1)->where('brand_slug',$slug)->firstOrFail();
+        return view('admin.brand.edit', compact('brand'));
+    }
+
+    public function update(Request $request){
+
+        $this->validate($request,[
+            'brand_name' => 'required',
+        ],[
+            'brand_name.required' => 'Enter Your Brand Name.'
+        ]);
+        $id = $request->brand_id;
+        $slug = Str::slug($request->brand_name, '-');
+        $editor = Auth::user()->id;
+
+        if($request->brand_feature == 'on'){
+            $brand_feature = 1;
+        }else{
+            $brand_feature = 0;
+        }
+
+        $update = Brand::where('brand_id',$id)->update([
+            'brand_name' => $request->brand_name,
+            'brand_slug' => $slug,
+            'brand_remarks' => $request->brand_remarks,
+            'brand_editor' => $editor,
+            'brand_feature' => $brand_feature,
+            'updated_at' => Carbon::now()->toDateTimeString()
+        ]);
+        if($request->hasFile('brand_image')){
+            $image = $request->file('brand_image');
+            $imageName = $id . time() . rand(1000,10000) . '-' . $image->getClientOriginalExtension();
+            Image::make($image)->resize(200,200)->save('uploads/brand/' . $imageName);
+
+            Brand::where('brand_id',$id)->update([
+                'brand_image' => $imageName,
+                'updated_at' => Carbon::now()->toDateTimeString(),
+            ]);
+        }
+        if($update){
+            Session::flash('success','successfully update partner');
+            return redirect()->back();
+        }else{
+            Session::flash('error','Opps! Failed to update.');
             return redirect()->back();
         }
     }
