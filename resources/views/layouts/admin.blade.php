39c35833137257bd7e4515ac0860eb98475278b6
unknown <asifshezan7@gmail.com>
2022-06-14 20:30:59 +0600

    Data Table Option Setup

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -255,9 +255,31 @@
         <script src="{{asset('contents/admin')}}/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js"></script>
         <!-- dashboard init -->
         <script src="{{asset('contents/admin')}}/js/pages/dashboard.init.js"></script>
-
         <script src="{{asset('contents/admin')}}/js/app.js"></script>
         <script src="{{asset('contents/admin')}}/js/custom.js"></script>
+
+
+        <!-- Required datatable js -->
+        <script src="{{asset('contents/admin')}}/libs/datatables.net/js/jquery.dataTables.min.js"></script>
+        <script src="{{asset('contents/admin')}}/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+        <!-- Buttons examples -->
+        <script src="{{asset('contents/admin')}}/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+        <script src="{{asset('contents/admin')}}/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js"></script>
+        <script src="{{asset('contents/admin')}}/libs/jszip/jszip.min.js"></script>
+        <script src="{{asset('contents/admin')}}/libs/pdfmake/build/pdfmake.min.js"></script>
+        <script src="{{asset('contents/admin')}}/libs/pdfmake/build/vfs_fonts.js"></script>
+        <script src="{{asset('contents/admin')}}/libs/datatables.net-buttons/js/buttons.html5.min.js"></script>
+        <script src="{{asset('contents/admin')}}/libs/datatables.net-buttons/js/buttons.print.min.js"></script>
+        <script src="{{asset('contents/admin')}}/libs/datatables.net-buttons/js/buttons.colVis.min.js"></script>
+
+        <!-- Responsive examples -->
+        <script src="{{asset('contents/admin')}}/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+        <script src="{{asset('contents/admin')}}/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
+
+        <!-- Datatable init js -->
+        <script src="{{asset('contents/admin')}}/js/pages/datatables.init.js"></script>
+
+        <script src="{{asset('contents/admin')}}/js/app.js"></script>
 
     </body>
 </html>
